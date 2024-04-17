ce44905a4b88d16ea138ccbc6ac072315e7a921f
bradenr402 <bradenr402@gmail.com>
2024-04-17 12:16:48 -0400

    refactor: remove curly braces

--- capitalize.test.js
+++ capitalize.test.js
@@ -1,5 +1,4 @@
 import capitalize from './capitalize';
 
-it('capitalizes the first letter of the first word', () => {
-  expect(capitalize('this is a test')).toBe('This is a test');
-});
+it('capitalizes the first letter of the first word', () =>
+  expect(capitalize('this is a test')).toBe('This is a test'));
--- reverse-string.test.js
+++ reverse-string.test.js
@@ -1,9 +1,7 @@
 import reverseString from './reverse-string';
 
-it('reverses a one word string', () => {
-  expect(reverseString('hello!')).toBe('!olleh');
-});
+it('reverses a one word string', () =>
+  expect(reverseString('hello!')).toBe('!olleh'));
 
-it('reverses a multiple word string', () => {
-  expect(reverseString('hello world!')).toBe('!dlrow olleh');
-});
+it('reverses a multiple word string', () =>
+  expect(reverseString('hello world!')).toBe('!dlrow olleh'));
